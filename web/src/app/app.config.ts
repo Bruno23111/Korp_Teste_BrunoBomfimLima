cadd9dfa3248ee2c4bc2b3eff8806dbe49e92b1a
bruno23111 <brunobomfimlima@gmail.com>
2026-08-18 16:09:45 -0300

    feat(web): refresh interface with PrimeNG

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,14 +1,40 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeuix/themes';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
+
+const KorpPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{violet.50}',
+      100: '{violet.100}',
+      200: '{violet.200}',
+      300: '{violet.300}',
+      400: '{violet.400}',
+      500: '{violet.500}',
+      600: '{violet.600}',
+      700: '{violet.700}',
+      800: '{violet.800}',
+      900: '{violet.900}',
+      950: '{violet.950}',
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(),
-    provideRouter(routes)
-  ]
+    providePrimeNG({ theme: { preset: KorpPreset, options: { darkModeSelector: '.app-dark' } } }),
+    provideRouter(routes),
+  ],
 };
